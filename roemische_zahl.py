96e02a54723626b08ac893c7c8869941d40a27c5
Martin Fischer <martin.jan.fischer@arcor.de>
2022-11-10 21:18:38 +0100

    Römische Zahl und Test Kommentare

--- roemische_zahl.py
+++ roemische_zahl.py
@@ -1,6 +1,16 @@
-def roemische_zahl(arabische_zahl): #Funktion roemische_zahl mit Parameter arabische_zahl
-    if arabische_zahl==0:           #Wenn arabische_zahl gleich Null ist
-        return ''                   #dann gebe einen leeren Text aus
-    elif arabische_zahl==1:         #Fals aber die arabische_zahl gleich Eins ist
-        return 'I'                  #dann gebe die Römische Zahl I als Text aus
+# Berechne die Römische Zahl aus einer Arabischen
 
+def roemische_zahl(arabische_zahl): # Definiere Funktion roemische_zahl mit Parameter arabische_zahl
+    if arabische_zahl==0:           # Wenn arabische_zahl gleich Null ist...
+        return ''                   # ... dann gebe einen leeren Text aus
+    elif arabische_zahl==1:         # Falls aber die arabische_zahl gleich Eins ist ...
+        return 'I'                  # ... dann gebe die Römische Zahl I als Text aus
+
+'''
+if __name__ == '__main__':          # Die obigen Funktionen werden nur ausgeführt...
+    import argparse
+    parser = argparse.ArgumentParser(description = 'Berechne die Römische Zahl aus einer Arabischen Zahl')
+    parser.add_argument('arabischezahl', type=int, help='Eine ganze Arabische Zahl')
+    args = parser.parse_args()
+    print(roemische_zahl(args.arabischezahl))   # ... wenn man sie hier in der main Funktion aufruft
+'''
--- roemische_zahl_test.py
+++ roemische_zahl_test.py
@@ -1,14 +1,19 @@
+# Teste roemische_zahl auf Korrektheit
+#
+# http://roemischezahlen.babuo.com/roemischen-ziffern-1-1000
+
 import unittest
 import roemische_zahl
 
-class roemische_zahl_test(unittest.TestCase):
-    def teste_roemische_zahl_0(self):               #definiere eine Testfunktion
-        ergebnis = roemische_zahl.roemische_zahl(0) #Rufe die Funktion roemische_zahl mit der arabischen Zahl 0 auf und speichere das Ergebnis
-        self.assertEqual(ergebnis, '')              #Vergleiche das ergebnis mit dem erwarteten Wert
+class roemische_zahl_test(unittest.TestCase):       # Definiere eine Test Klasse
+    
+    def teste_roemische_zahl_0(self):               # Definiere eine Testfunktion
+        ergebnis = roemische_zahl.roemische_zahl(0) # Rufe die Funktion roemische_zahl mit einer Arabischen Zahl auf und speichere das Ergebnis
+        self.assertEqual(ergebnis, '')              # Vergleiche das Ergebnis mit der erwarteten Römischen Zahl
+    
+    def teste_roemische_zahl_1(self):               # Definiere eine Testfunktion
+        ergebnis = roemische_zahl.roemische_zahl(1) # Rufe die Funktion roemische_zahl mit einer arabischen Zahl auf und speichere das Ergebnis
+        self.assertEqual(ergebnis, 'I')             # Vergleiche das Ergebnis mit der erwarteten Römischen Zahl
 
-    def teste_roemische_zahl_1(self):               #definiere eine Testfunktion
-        ergebnis = roemische_zahl.roemische_zahl(1) #Rufe die Funktion roemische_zahl mit der arabischen Zahl 1 auf und speichere das Ergebnis
-        self.assertEqual(ergebnis, 'I')             #Vergleiche das ergebnis mit dem erwarteten Wert
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == '__main__':  # Die obigen Funktionen werden nur ausgeführt...
+    unittest.main()         # ... wenn man sie hier in der main Funktion aufruft
