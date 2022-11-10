9eea734a155d9c14362cad699a9a9970df775372
Martin Fischer <martin.jan.fischer@arcor.de>
2022-11-10 18:22:29 +0100

    Römische Zahl Kommentare

--- roemische_zahl.py
+++ roemische_zahl.py
@@ -1,6 +1,6 @@
-def roemische_zahl(arabische_zahl):
-    if arabische_zahl==0:
-        return ''
-    elif arabische_zahl==1:
-        return 'I'
+def roemische_zahl(arabische_zahl): #Funktion roemische_zahl mit Parameter arabische_zahl
+    if arabische_zahl==0:           #Wenn arabische_zahl gleich Null ist
+        return ''                   #dann gebe einen leeren Text aus
+    elif arabische_zahl==1:         #Fals aber die arabische_zahl gleich Eins ist
+        return 'I'                  #dann gebe die Römische Zahl I als Text aus
 
--- roemische_zahl_test.py
+++ roemische_zahl_test.py
@@ -2,13 +2,13 @@ import unittest
 import roemische_zahl
 
 class roemische_zahl_test(unittest.TestCase):
-    def teste_roemische_zahl_0(self):
-        ergebnis = roemische_zahl.roemische_zahl(0)
-        self.assertEqual(ergebnis, '')
+    def teste_roemische_zahl_0(self):               #definiere eine Testfunktion
+        ergebnis = roemische_zahl.roemische_zahl(0) #Rufe die Funktion roemische_zahl mit der arabischen Zahl 0 auf und speichere das Ergebnis
+        self.assertEqual(ergebnis, '')              #Vergleiche das ergebnis mit dem erwarteten Wert
 
-    def teste_roemische_zahl_1(self):
-        ergebnis = roemische_zahl.roemische_zahl(1)
-        self.assertEqual(ergebnis, 'I')
+    def teste_roemische_zahl_1(self):               #definiere eine Testfunktion
+        ergebnis = roemische_zahl.roemische_zahl(1) #Rufe die Funktion roemische_zahl mit der arabischen Zahl 1 auf und speichere das Ergebnis
+        self.assertEqual(ergebnis, 'I')             #Vergleiche das ergebnis mit dem erwarteten Wert
 
 if __name__ == '__main__':
     unittest.main()
