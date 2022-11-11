66faab836d7501c7acadb6f16cc7e5c6d96029b1
Martin Fischer <martin.jan.fischer@arcor.de>
2022-11-11 19:55:40 +0100

    Römische Zahl und Test Kommentaren

--- roemische_zahl.py
+++ roemische_zahl.py
@@ -1,10 +1,14 @@
 # Berechne die Römische Zahl aus einer Arabischen Zahl
 
-def roemische_zahl(arabische_zahl): # Definiere Funktion "roemische_zahl" mit Parameter-Variablen "arabische_zahl"
-    if arabische_zahl==0:           # Wenn Variable arabische_zahl gleich Null ist ...
-        return ''                   # ... dann gebe einen leeren Zeichenketten-Text zurück
-    elif arabische_zahl==1:         # Ansonsten, wenn die Variable arabische_zahl gleich Eins ist ...
-        return 'I'                  # ... dann gebe die Römische Zahl I als Zeichenketten-Text zurück
+# Definiere Funktion "berechne_roemische_zahl"
+# mit Parameter-Variablen "arabische_zahl"
+def berechne_roemische_zahl(arabische_zahl):
+    roemische_zahl = ''         # Definiere Variable "roemische_zahl" die einen leeren Zeichenketten-Text enthält
+    if arabische_zahl==0:       # Wenn Variable "arabische_zahl" gleich Null ist ...
+        roemische_zahl = ''     # ... dann bekommt die Variable "roemische_zahl" einen leeren Zeichenketten-Text
+    elif arabische_zahl==1:     # Ansonsten, wenn die Variable "arabische_zahl" gleich Eins ist ...
+        roemische_zahl = 'I'    # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'I'
+    return roemische_zahl       # Gebe den Wert von der Variablen "roemische_zahl" zurück
 
 '''
 if __name__ == '__main__':
@@ -13,7 +17,7 @@ if __name__ == '__main__':
     parser.add_argument('arabische_zahl', type=int, help='Eine natürliche Arabische Zahl')
     args = parser.parse_args()
     
-    # Die obige Funktion "roemische_zahl" wird nur dann in der Kommandozeile ausgeführt...
+    # Die obige Funktion "berechne_roemische_zahl" wird nur dann in der Kommandozeile ausgeführt...
     # ... wenn man sie hier in der __main__ Funktion aufruft
-    print(roemische_zahl(args.arabische_zahl))
+    print(berechne_roemische_zahl(args.arabische_zahl))
 '''
--- roemische_zahl_test.py
+++ roemische_zahl_test.py
@@ -3,16 +3,16 @@
 # https://www.mathe-ist-einfach.de/Roemischezahlen/rom1000.html
 
 import unittest
-from roemische_zahl import roemische_zahl
+from roemische_zahl import berechne_roemische_zahl
 
 class roemische_zahl_test(unittest.TestCase):   # Definiere die Test Klasse "roemische_zahl_test"
     
     def teste_roemische_zahl_0(self):           # Definiere die Testfunktion "teste_roemische_zahl_0"
-        ergebnis = roemische_zahl(0)            # Rufe die Funktion roemische_zahl mit 0 auf und speichere den Rückgabewert in der Variablen "ergebnis"
+        ergebnis = berechne_roemische_zahl(0)   # Rufe die Funktion berechne_roemische_zahl mit 0 auf und speichere den Rückgabewert in der Variablen "ergebnis"
         self.assertEqual(ergebnis, '')          # Melde einen Fehler wenn die Variable ergebnis nicht den leeren Zeichenketten-Text enthält
     
     def teste_roemische_zahl_1(self):           # Definiere die Testfunktion "teste_roemische_zahl_1"
-        ergebnis = roemische_zahl(1)            # Rufe die Funktion roemische_zahl mit 1 auf und speichere den Rückgabewert in der Variablen "ergebnis"
+        ergebnis = berechne_roemische_zahl(1)   # Rufe die Funktion berechne_roemische_zahl mit 1 auf und speichere den Rückgabewert in der Variablen "ergebnis"
         self.assertEqual(ergebnis, 'I')         # Melde einen Fehler wenn die Variable ergebnis nicht den Zeichenketten-Text I enthält
 
 if __name__ == '__main__':  # Die obigen Funktionen werden nur dann in der Kommandozeile ausgeführt...
