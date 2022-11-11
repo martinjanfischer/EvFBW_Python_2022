5ea49ec9d6197e45f81f848163234b4bc3359c60
Martin Fischer <martin.fischer@ubisoft.com>
2022-11-11 10:04:01 +0100

    Römische Zahl Test

--- roemische_zahl_test.py
+++ roemische_zahl_test.py
@@ -1,6 +1,6 @@
 # Teste das Programm "roemische_zahl" dass es funktioniert
 #
-# http://roemischezahlen.babuo.com/roemischen-ziffern-1-1000
+# https://www.mathe-ist-einfach.de/Roemischezahlen/rom1000.html
 
 import unittest
 from roemische_zahl import roemische_zahl
@@ -9,11 +9,11 @@ class roemische_zahl_test(unittest.TestCase):   # Definiere die Test Klasse "roe
     
     def teste_roemische_zahl_0(self):           # Definiere die Testfunktion "teste_roemische_zahl_0"
         ergebnis = roemische_zahl(0)            # Rufe die Funktion roemische_zahl mit 0 auf und speichere den Rückgabewert in der Variablen "ergebnis"
-        self.assertEqual(ergebnis, '')          # Vergleiche das Ergebnis mit der erwarteten Römischen Zahl
+        self.assertEqual(ergebnis, '')          # Melde einen Fehler wenn die Variable ergebnis nicht den leeren Zeichenketten-Text enthält
     
     def teste_roemische_zahl_1(self):           # Definiere die Testfunktion "teste_roemische_zahl_1"
         ergebnis = roemische_zahl(1)            # Rufe die Funktion roemische_zahl mit 1 auf und speichere den Rückgabewert in der Variablen "ergebnis"
-        self.assertEqual(ergebnis, 'I')         # Vergleiche das Ergebnis mit der erwarteten Römischen Zahl
+        self.assertEqual(ergebnis, 'I')         # Melde einen Fehler wenn die Variable ergebnis nicht den Zeichenketten-Text I enthält
 
 if __name__ == '__main__':  # Die obigen Funktionen werden nur dann in der Kommandozeile ausgeführt...
     unittest.main()         # ... wenn man sie hier in der __main__ Funktion aufruft
