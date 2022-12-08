ca06094587706e8c75619ab3c7726483eafe4651
Martin Fischer <martin.fischer@ubisoft.com>
2022-12-08 15:49:26 +0100

    Kursvorbereitung Römische Zahl

--- roemische_zahl.py
+++ roemische_zahl.py
@@ -6,34 +6,34 @@ def berechne_roemische_zahl(arabische_zahl):
     roemische_zahl = ''         # Definiere Variable "roemische_zahl" die einen leeren Zeichenketten-Text enthält
     
     if arabische_zahl==10:      # Wenn die Variable "arabische_zahl" gleich 10 ist ...
-        roemische_zahl = 'X'    # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'X'
+        roemische_zahl += 'X'   # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'X'
         arabische_zahl -= 10
     if arabische_zahl==9:       # Wenn die Variable "arabische_zahl" gleich 9 ist ...
-        roemische_zahl = 'IX'   # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'IX'
+        roemische_zahl += 'IX'  # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'IX'
         arabische_zahl -= 9
     if arabische_zahl==8:       # Wenn die Variable "arabische_zahl" gleich 8 ist ...
-        roemische_zahl = 'VIII' # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'VIII'
+        roemische_zahl += 'VIII'# ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'VIII'
         arabische_zahl -= 8
     if arabische_zahl==7:       # Wenn die Variable "arabische_zahl" gleich 7 ist ...
-        roemische_zahl = 'VII'  # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'VII'
+        roemische_zahl += 'VII' # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'VII'
         arabische_zahl -= 7
     if arabische_zahl==6:       # Wenn die Variable "arabische_zahl" gleich 6 ist ...
-        roemische_zahl = 'VI'   # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'VI'
+        roemische_zahl += 'VI'  # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'VI'
         arabische_zahl -= 6
     if arabische_zahl==5:       # Wenn die Variable "arabische_zahl" gleich 5 ist ...
-        roemische_zahl = 'V'    # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'V'
+        roemische_zahl += 'V'   # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'V'
         arabische_zahl -= 5
     if arabische_zahl==4:       # Wenn die Variable "arabische_zahl" gleich 4 ist ...
-        roemische_zahl = 'IV'   # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'IV'
+        roemische_zahl += 'IV'  # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'IV'
         arabische_zahl -= 4
     if arabische_zahl==3:       # Wenn die Variable "arabische_zahl" gleich 3 ist ...
-        roemische_zahl = 'III'  # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'III'
+        roemische_zahl += 'III' # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'III'
         arabische_zahl -= 3
     if arabische_zahl==2:       # Wenn die Variable "arabische_zahl" gleich 2 ist ...
-        roemische_zahl = 'II'   # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'II'
+        roemische_zahl += 'II'  # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'II'
         arabische_zahl -= 2
     if arabische_zahl==1:       # Wenn die Variable "arabische_zahl" gleich 1 ist ...
-        roemische_zahl = 'I'    # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'I'
+        roemische_zahl += 'I'   # ... dann bekommt die Variable "roemische_zahl" den Zeichenketten-Text 'I'
         arabische_zahl -= 1
     
     return roemische_zahl       # Gebe den Wert von der Variablen "roemische_zahl" zurück
