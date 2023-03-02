7974b8bfcc85034410d8e8f1ec10a40df4687989
Martin Fischer <martin.jan.fischer@arcor.de>
2023-03-02 18:07:57 +0100

    Veränderungen während des Kurses, neues Raumschiff Bild gemalt von SleepyKitten010

--- dimorphos/dimorphos.py
+++ dimorphos/dimorphos.py
@@ -46,6 +46,7 @@ class Dimorphos:
         start_ansicht.raumschiffe.append(Raumschiff(Vector2(0, 0), "raumschiff_sui", self.laser_bild, [Vector2(-11, -1),Vector2(11, -1)]))
         start_ansicht.raumschiffe.append(Raumschiff(Vector2(0, 0), "raumschiff_von_konrad", self.laser_bild, [Vector2(16, -25),Vector2(-16, -25), Vector2(39,-12), Vector2(-39,-12)]))
         start_ansicht.raumschiffe.append(Raumschiff(Vector2(0, 0), "raumschiff.perfect_grafic", self.laser_bild, [Vector2(0, -25)]))
+        start_ansicht.raumschiffe.append(Raumschiff(Vector2(0, 0), "raumschiff_Mouthfullmod",self.laser_bild, [Vector2(0, -44)]))
         
         # Füge neue Asteroidenbilder in die Liste der Ansichten hinzu
         bild_asteroid_1 = lade_bild("asteroid")
